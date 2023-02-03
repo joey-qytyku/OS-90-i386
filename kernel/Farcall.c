1aee5625a066e4051e9f28b08d89efa69a06a294
joey-qytyku <100374369+joey-qytyku@users.noreply.github.com>
2023-02-03 14:28:14 -0500

    Update Farcall.c

--- kernel/Farcall.c
+++ kernel/Farcall.c
@@ -1,9 +1,17 @@
 #include <>
 
-typedef DOS_FAR_CALL_HANDLER;
 
 //
-// To make the interface simpler, the V86 chain link is embedded
+// Far call handler does not need to return anything. The kernel
+// knows exactly which one to call by index
+//
+typedef VOID (*DOS_FCALL_HANDLER) (PTRAP_FRAME*);
+
+//
+// To make the interface simpler, the V86 chain link struct
+// is embedded here. This structure is part of a linked list.
+// The difference is that it is indexed by number rather than probing
+// functions iteratively as with V86.
 //
 
 typedef struct {
@@ -15,8 +23,14 @@ typedef struct {
 static WORD current_farcall = 0;
 const PVOID bios_rom_space = 0xF0000;
 
+
+//
+// This function is called by the page fault handler when the proper
+// conditions are met.
+//
 HandleFcallAfterPF(WORD eip)
 {
+  // EIP represent
 }
 
 
@@ -25,10 +39,9 @@ HandleFcallAfterPF(WORD eip)
 // Regular DOS programs may access XMS and other far call API's. The
 // first step is to capture the multiplex interrupt
 //
-// address_report_vint_link: A V86 chain link that reports
-// fcall_conf_struct: A structure to hold the handler address
-//
 ScCreateDosFarCallHandler(
-                          )
-{
+                          FARCALL_CONF  fcall_cfg,
+                          STATUS (*report_handler)(),
+                          DOS_FCALL_HND fcall_handler;
+){
 }
